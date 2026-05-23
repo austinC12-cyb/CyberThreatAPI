44ff88b23f5425470dbe05df32bc2c16f50a8c57
StarwyGidsz <starwy888@gmail.com>
2026-05-23 21:49:38 +0800

    Step 1 Test Route and FastAPI Import

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # TODO: Step 1 - Import FastAPI and set up the first test route.
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"active_threats": 5, "Welcome TO": "Cyber Threat API"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
